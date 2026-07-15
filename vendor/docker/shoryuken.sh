1250bd05d4560a10ac0bcade1c8c92cac14c2448
kaysiz <kuda.siziva@datacite.org>
2026-07-15 13:40:38 +0200

    shoryuken: only start when AWS_REGION is set

--- vendor/docker/shoryuken.sh
+++ vendor/docker/shoryuken.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
-set -e
-
-# Standard Shoryuken runner for DataCite services
-exec /sbin/setuser app bundle exec shoryuken -R -C config/shoryuken.yml
+cd /home/app/webapp
+exec 2>&1
+# Start only in AWS; allow opt-out with DISABLE_QUEUE_WORKER (e.g. local compose)
+if [ -n "$AWS_REGION" ] && [ -z "$DISABLE_QUEUE_WORKER" ]; then
+  exec /sbin/setuser app bundle exec shoryuken -R -C config/shoryuken.yml
+fi
